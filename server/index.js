0ff55c92d33b2be50ffd8c9b883afe77ef29b6b4
Sagar Kapoor <sagarbadal70@gmail.com>
2024-01-29 19:23:55 +0530

    Done with authorization

--- server/index.js
+++ server/index.js
@@ -1,38 +1,59 @@
-const express=require('express');
-const bodyParser=require('body-parser');
-const methodoverride=require('method-override')
-const mongoose=require('mongoose');
-const cors=require('cors');
-const dotenv=require('dotenv');
-const morgan=require('morgan');
-const multer=require('multer');
-const helmet=require('helmet');
-const path=require('path');
-const fileURLToPath=require('url');
-// Middleware or Configurations
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+// import userRoutes from "./routes/users.js";
+// import postRoutes from "./routes/posts.js";
+import { register } from "./controllers/auth.js";
+// import { createPost } from "./controllers/posts.js";
+// import { verifyToken } from "./middleware/auth.js";
+// import User from "./models/User.js";
+// import Post from "./models/Post.js";
+// import { users, posts } from "./data/index.js";
+
+/* CONFIGURATIONS */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));   //for cross origin resouce sharing
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use(methodoverride("_method"));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
-// File Storage
+
+/* FILE STORAGE */
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "public/assets");
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname);
-    },
-  });
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
 const upload = multer({ storage });
-// MongoDB and Mongoose Setup
-const PORT=process.env.PORT || 3000;
+
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+// app.post("/posts", verifyToken, upload.single("picture"), createPost);
+
+/* ROUTES */
+app.use("/auth", authRoutes);
+// app.use("/users", userRoutes);
+// app.use("/posts", postRoutes);
+console.log(process.env.PORT+" "+process.env.MONGO_URL);
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
 mongoose
   .connect(process.env.MONGO_URL, {
   })
@@ -44,4 +65,3 @@ mongoose
     // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
-
