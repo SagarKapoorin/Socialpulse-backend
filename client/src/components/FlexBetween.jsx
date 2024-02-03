bb8daedcc8d64cfe34a312750dd45b039ac96d88
Sagar Kapoor <sagarbadal70@gmail.com>
2024-02-03 14:57:22 +0530

    work

--- client/src/components/FlexBetween.jsx
+++ client/src/components/FlexBetween.jsx
@@ -1,6 +1,6 @@
 import { Box } from "@mui/material";
 import { styled } from "@mui/system";
-
+// making our own material ui components
 const FlexBetween = styled(Box)({
   display: "flex",
   justifyContent: "space-between",
