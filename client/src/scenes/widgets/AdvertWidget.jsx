75f7c9628eeae02efd45163bbc512e32a26ae074
Sagar Kapoor <sagarbadal70@gmail.com>
2024-02-07 19:11:40 +0530

    fixing typo

--- client/src/scenes/widgets/AdvertWidget.jsx
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -20,16 +20,15 @@ const AdvertWidget = () => {
         width="100%"
         height="auto"
         alt="advert"
-        src="http://localhost:3001/assets/info4.jpeg"
+        src="http://localhost:8080/assets/info4.jpeg"
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
-        <Typography color={main}>MikaCosmetics</Typography>
-        <Typography color={medium}>mikacosmetics.com</Typography>
+        <Typography color={main}>Magic-Cosmetics</Typography>
+        <Typography color={medium}>magiccosmetics.com</Typography>
       </FlexBetween>
       <Typography color={medium} m="0.5rem 0">
-        Your pathway to stunning and immaculate beauty and made sure your skin
-        is exfoliating skin and shining like light.
+      I put on makeup to distract people from the fact that I haven't slept in days.
       </Typography>
     </WidgetWrapper>
   );
