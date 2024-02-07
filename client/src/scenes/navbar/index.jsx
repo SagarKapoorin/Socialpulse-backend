040ef08928ecd1cf31c646a6978063d484de863b
Sagar Kapoor <sagarbadal70@gmail.com>
2024-02-07 17:57:18 +0530

    Made few custom components

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          SocialPulse
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
