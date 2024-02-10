91fafc30e7685a6d47db06097c57652b10a40dfa
Sagar Kapoor <sagarbadal70@gmail.com>
2024-02-10 17:54:54 +0530

    done with share button

--- client/src/components/Share.jsx
+++ client/src/components/Share.jsx
@@ -3,10 +3,42 @@ import TelegramIcon from '@mui/icons-material/Telegram';
 import { Box, Divider, Icon, IconButton, useTheme } from "@mui/material";
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import FlexBetween from './FlexBetween';
+import { blue } from '@mui/material/colors';
 const ShareC=()=>{
     const { palette } = useTheme();
     const main = palette.neutral.main;
     const primary = palette.primary.main;
+    const shareOnX = () => {
+      const currentUrl = encodeURIComponent(window.location.href);
+    const twitterUrl = `https://twitter.com/intent/tweet?url=${currentUrl}&text=Check out this awesome website!`;
+    window.open(twitterUrl, '_blank');
+    };
+    const shareOnFacebook=()=>{
+      const websiteUrl = encodeURIComponent(window.location.href);
+      const facebookShareLink = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(websiteUrl)}`;
+      window.open(facebookShareLink, '_blank');
+    }
+    const shareOnInstagram=()=>{
+      const photoUrl = encodeURIComponent('https://example.com/photo.jpg');
+      const caption = encodeURIComponent('Check out this awesome photo!');
+      const instagramUrl = `https://www.instagram.com/`;
+    window.open(instagramUrl, '_blank');
+    }
+    const shareOnWhatsapp=()=>{
+      const message = encodeURIComponent('Check out this awesome website!');
+  const whatsappUrl = `https://wa.me/?text=${message}`;
+  window.open(whatsappUrl, '_blank');
+    }
+    const shareOnTelegram=()=>{
+      const websiteUrl = encodeURIComponent(window.location.href);
+      const telegramShareLink = `https://t.me/share/url?url=${encodeURIComponent(websiteUrl)}`;
+      window.open(telegramShareLink, '_blank');
+    }
+    const shareOnLinkedIn=()=>{
+      const currentUrl = encodeURIComponent(window.location.href);
+    const linkedInUrl = `https://www.linkedin.com/sharing/share-offsite/?url=${currentUrl}`;
+    window.open(linkedInUrl, '_blank');
+    }
     return(
         <Box
         sx={{
@@ -24,28 +56,28 @@ const ShareC=()=>{
       >
         
         <FlexBetween  mt="0.25rem" gap="0.3rem">
-        <IconButton>
-        <WhatsApp sx={{ color: main }}/>
+        <IconButton onClick={shareOnWhatsapp}>
+        <WhatsApp sx={{ color: "#25d366" }}/>
         </IconButton>
         <Divider orientation="vertical" flexItem sx={{ color: main }}/>
-        <IconButton>
-        <Facebook sx={{ color: main }}/>
+        <IconButton onClick={shareOnFacebook}>
+        <Facebook sx={{ color: "#316FF6" }}/>
         </IconButton>
         <Divider orientation="vertical" flexItem sx={{ color: main }}/>
-        <IconButton>
+        <IconButton onClick={shareOnX}>
         <X sx={{ color: main }}/>
         </IconButton>
         <Divider orientation="vertical" flexItem sx={{ color: main }}/>
-        <IconButton>
-        <Instagram sx={{ color: main }}/>
+        <IconButton onClick={shareOnInstagram}>
+        <Instagram sx={{ color: "#d62976" }}/>
         </IconButton>
         <Divider orientation="vertical" flexItem sx={{ color: main }} />
-        <IconButton>
-        <TelegramIcon sx={{ color: main }}/>
+        <IconButton onClick={shareOnTelegram}>
+        <TelegramIcon sx={{ color: "#0088cc" }}/>
         </IconButton>
         <Divider orientation="vertical" flexItem sx={{ color: main }} />
-        <IconButton>
-        <LinkedInIcon sx={{ color: main }}/>
+        <IconButton onClick={shareOnLinkedIn}>
+        <LinkedInIcon sx={{ color: "#0A66C2" }}/>
         </IconButton>
         </FlexBetween>
       </Box>
